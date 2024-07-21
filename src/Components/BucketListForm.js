7a203cbd4b4971f6b141c170a7cd73b5d17e7a2f
sgoodridge96 <sgoodridge96@gmail.com>
2024-07-21 01:51:49 -0400

    feat: Items added saved in array.

--- src/Components/BucketListForm.js
+++ src/Components/BucketListForm.js
@@ -1,12 +1,21 @@
-import React from 'react'
+import React, {useState} from 'react'
 
-export const BucketListForm = () => {
+export const BucketListForm = ({addToList}) => {
+  const [value, setValue] = useState("")
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    addToList(value)
+    // console.log(value)
+
+  }
+
   return (
-  
-    <form className='BucketListForm'>
+
+    <form className='BucketListForm' onSubmit={handleSubmit}>
       <input type="text" className='bucket-list-input' 
-      placeholder='Enter something on your bucket list'/>
-      <button type='submit' className='bucket-list-btn'>
+      placeholder='Enter' onChange={(e) => setValue(e.target.value)}/>
+      <button type='submit' className='bucket-list-btn'>Add Item
       </button>
     </form>
   
--- src/Components/BucketListWrapper.js
+++ src/Components/BucketListWrapper.js
@@ -1,10 +1,20 @@
-import React from 'react'
+import React, {useState} from 'react'
 import { BucketListForm } from './BucketListForm'
+import {v4 as uuidv4} from 'uuid';
+uuidv4();
 
 export const BucketListWrapper = () => {
+  const [bktList, setBktList] = useState([])
+
+  const addToList = listItem => {
+    setBktList([...bktList, {id: uuidv4(), task: listItem,
+    completed: false, isEditing: false}])
+  }
+  
   return (
+    // console.log(bktList),
     <div className='BucketListWrapper'>
-      <BucketListForm />
+      <BucketListForm addToList={addToList} />
     </div>
   )
 }
