798651d3861f706e546df4d43fa0283334004620
sgoodridge96 <sgoodridge96@gmail.com>
2024-07-23 01:11:17 -0400

    feat: user can click on bucket list item to put a line through it

--- src/Components/BucketList.js
+++ src/Components/BucketList.js
@@ -1,14 +1,23 @@
 import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faTrashArrowUp } from "@fortawesome/free-solid-svg-icons";
-import { faPenToSquare } from "@fortawesome/free-solid-svg-icons";
+import {
+  faPenToSquare,
+  faSquareCheck,
+  faTrashArrowUp,
+} from "@fortawesome/free-solid-svg-icons";
 
-export const BucketList = ({ addItem }) => {
+export const BucketList = ({ addItem, toggleComplete }) => {
   return (
     <div className="BucketList">
-      {/* */}
-      <p>{addItem.addItem}</p>
+      {/*When add item button is hit, add the item to the bucket list */}
+      <p
+        onClick={() => toggleComplete(addItem.id)}
+        className={`${addItem.completed ? "completed" : ""}`}
+      >
+        {addItem.addItem}
+      </p>
       <div>
+        <FontAwesomeIcon icon={faSquareCheck} className="icon" />
         <FontAwesomeIcon icon={faPenToSquare} className="icon" />
         <FontAwesomeIcon icon={faTrashArrowUp} className="icon" />
       </div>
