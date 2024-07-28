7b411d306e8cccfb48bc28073a3cf40f8fe2bc5e
sgoodridge96 <sgoodridge96@gmail.com>
2024-07-28 02:59:25 -0400

    feat: defaults to users location

--- src/Components/Map.js
+++ src/Components/Map.js
@@ -1,20 +1,33 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { MapContainer, Marker, TileLayer } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
 import "./leafletConfig";
 
 export const Map = () => {
+  const [currentLocation, setCurrentLocation] = useState(null);
+
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition((position) => {
+      setCurrentLocation({
+        lat: position.coords.latitude,
+        lng: position.coords.longitude,
+      });
+    });
+  }, []);
+
   return (
-    <MapContainer
-      center={[51.505, -0.09]}
-      zoom={13}
-      style={{ height: "100%", width: "100%" }}
-    >
-      <TileLayer
-        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-      />
-      <Marker position={[51.505, -0.09]}></Marker>
-    </MapContainer>
+    currentLocation && (
+      <MapContainer
+        center={currentLocation}
+        zoom={13}
+        style={{ height: "100%", width: "100%" }}
+      >
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        <Marker position={currentLocation}></Marker>
+      </MapContainer>
+    )
   );
 };
