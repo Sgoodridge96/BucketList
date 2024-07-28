7a5ad7b3e0b2745bcf71b92040d4134b5d53d131
sgoodridge96 <sgoodridge96@gmail.com>
2024-07-28 03:32:12 -0400

    styling and code cleanup

--- src/Components/Map.js
+++ src/Components/Map.js
@@ -16,18 +16,20 @@ export const Map = () => {
   }, []);
 
   return (
-    currentLocation && (
-      <MapContainer
-        center={currentLocation}
-        zoom={13}
-        style={{ height: "100%", width: "100%" }}
-      >
-        <TileLayer
-          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        />
-        <Marker position={currentLocation}></Marker>
-      </MapContainer>
-    )
+    <div className="current-location">
+      {currentLocation && (
+        <MapContainer
+          center={currentLocation}
+          zoom={13}
+          style={{ height: "100%", width: "100%" }}
+        >
+          <TileLayer
+            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          />
+          <Marker position={currentLocation}></Marker>
+        </MapContainer>
+      )}
+    </div>
   );
 };
